3816f7265e559a9719b8ba412ca40794d26ca7f2
Julian <me@julian.pro>
2020-08-01 11:19:38 +0200

    feat(colors-neutral.js): neutral color palette added to token-system

---
+++ packages/tokens/src/tokens/colors/colors-neutral.js
@@ -0,0 +1,23 @@
+const colorsNeutral = {
+    'color-neutral-900': '#091E42',
+    'color-neutral-800': '#172B4D',
+    'color-neutral-700': '#253858',
+    'color-neutral-600': '#344563',
+    'color-neutral-500': '#42526E',
+    'color-neutral-400': '#505F79',
+    'color-neutral-300': '#5E6C84',
+    'color-neutral-200': '#6B778C',
+    'color-neutral-100': '#7A869A',
+    'color-neutral-90': '#8993A4',
+    'color-neutral-80': '#97A0AF',
+    'color-neutral-70': '#A5ADBA',
+    'color-neutral-60': '#B3BAC5',
+    'color-neutral-50': '#C1C7D0',
+    'color-neutral-40': '#DFE1E6',
+    'color-neutral-30': '#EBECF0',
+    'color-neutral-20': '#F4F5F7',
+    'color-neutral-10': '#FAFBFC',
+    'color-neutral-0': '#FFFFFF',
+};
+
+module.exports = colorsNeutral;
