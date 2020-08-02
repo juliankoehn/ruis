872a361456729b5990b58cd30ac9095fd6892030
Julian <me@julian.pro>
2020-08-02 10:34:56 +0200

    feat(semantic colors): extended semantic color palette: warning; info

--- packages/tokens/src/tokens/colors/colors-semantic.js
+++ packages/tokens/src/tokens/colors/colors-semantic.js
@@ -1,8 +1,9 @@
 const colorsSemantic = {
     'color-primary': '#2E75D4',
-    'color-positive': '#16875D',
-    'color-negative': '#BF3045',
-    'color-warning': '#F79B0C',
+    'color-positive': '#23d160',
+    'color-negative': '#ff3860',
+    'color-warning': '#ffdd57',
+    'color-info': '#209cee',
 };
 
 module.exports = colorsSemantic;
