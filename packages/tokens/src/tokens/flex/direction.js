fcdd4546d69e13592a37395f87b04c4ac5149548
Julian <me@julian.pro>
2020-08-03 20:56:23 +0200

    feat(flex-direction): added flex-direction to tokens

---
+++ packages/tokens/src/tokens/flex/direction.js
@@ -0,0 +1,8 @@
+const FlexDirection = {
+  'flex-row': 'row',
+  'flex-row-reverse': 'row-reverse',
+  'flex-col': 'column',
+  'flex-col-reverse': 'column-reverse',
+};
+
+module.exports = FlexDirection;
