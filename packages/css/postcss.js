0d72862f056bc02ba56f2b82504a766ed839cc96
Julian <me@julian.pro>
2020-08-03 20:37:29 +0200

    fix(width): added response width component to css library

--- packages/css/postcss.js
+++ packages/css/postcss.js
@@ -1,4 +1,5 @@
 const postcss = require('postcss');
+const mediaVariables = require('postcss-media-variables');
 const cssvariables = require('postcss-css-variables');
 const calc = require('postcss-calc');
 
@@ -7,12 +8,15 @@ const fs = require('fs');
 const css = fs.readFileSync('dist/styles.css', 'utf8');
 
 // Process your CSS with postcss-css-variables
-const output = postcss([cssvariables()])
-    .use(calc())
-    .process(css).css;
-fs.writeFile('dist/styles.css', output, err => {
-    if (err) {
-        throw err;
-    }
-    console.log('CSS successfully compiled!'); /* eslint-disable-line */
+const output = postcss()
+  .use(mediaVariables()) // must be first and last
+  .use(cssvariables())
+  .use(calc())
+  .use(mediaVariables()) // must be first and last
+  .process(css).css;
+fs.writeFile('dist/styles.css', output, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('CSS successfully compiled!'); /* eslint-disable-line */
 });
