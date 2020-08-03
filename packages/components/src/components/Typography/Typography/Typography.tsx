529152d0ecc46e0aa18ccf8204a733cd16c0a273
Julian <me@julian.pro>
2020-08-03 22:31:40 +0200

    fix(typography): allowing to apply settings on typography context

--- packages/components/src/components/Typography/Typography/Typography.tsx
+++ packages/components/src/components/Typography/Typography/Typography.tsx
@@ -1,63 +1,47 @@
-import React from 'react'
-import { TypographyProps, defaultProps } from './types'
-import { css } from '@emotion/core'
+import React from 'react';
+import {
+  TypographyProps,
+  defaultProps,
+  TypographyConfiguration,
+} from './types';
+import { css } from '@emotion/core';
 
 const defaultConfiguration: TypographyConfiguration = {
-    heading: { spacing: 'spacing2' },
-    paragraph: { spacing: 'spacing2' },
-}
+  heading: { spacing: 'spacing2' },
+  paragraph: { spacing: 'spacing2' },
+};
 
-export type Spacings =
-    'spacingPx'
-    | 'spacing0'
-    | 'spacing1'
-    | 'spacing2'
-    | 'spacing3'
-    | 'spacing4'
-    | 'spacing5'
-    | 'spacing6'
-    | 'spacing8'
-    | 'spacing10'
-    | 'spacing12'
-    | 'spacing16'
-    | 'spacing20'
-    | 'spacing24'
-    | 'spacing32'
-    | 'spacing40'
-    | 'spacing48'
-    | 'spacing56'
-    | 'spacing64'
-
-export type ElementConfig = {
-    spacing: Spacings
-}
-
-export interface TypographyConfiguration {
-    heading: ElementConfig
-    paragraph: ElementConfig
-}
-
-export const TypographyContext = React.createContext<TypographyConfiguration>(defaultConfiguration)
+export const TypographyContext = React.createContext<TypographyConfiguration>(
+  defaultConfiguration,
+);
 
 export class Typography extends React.Component<TypographyProps> {
-    static defaultProps = defaultProps
+  static defaultProps = defaultProps;
 
-    render() {
-        const { className, children, testId, ...otherProps } = this.props
+  render() {
+    const {
+      className,
+      children,
+      testId,
+      configuration,
+      ...otherProps
+    } = this.props;
 
-        return (
-            <TypographyContext.Provider value={defaultConfiguration}>
-                <div
-                    {...otherProps}
-                    className={className}
-                    data-test-id={testId}
-                    css={css({
-                        display: 'block'
-                    })}
-                >
-                    {children}
-                </div>
-            </TypographyContext.Provider>
-        )
-    }
+    const config = configuration ? configuration : defaultConfiguration;
+
+    return (
+      <TypographyContext.Provider value={config}>
+        <div
+          {...otherProps}
+          className={className}
+          data-test-id={testId}
+          css={css({
+            display: 'block',
+          })}
+        >
+          {children}
+        </div>
+      </TypographyContext.Provider>
+    );
+  }
 }
--- packages/components/src/components/Typography/Typography/types.ts
+++ packages/components/src/components/Typography/Typography/types.ts
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-export const defaultProps = {
-    testId: 'ruids-text-container',
+export type Spacings =
+  | 'spacingPx'
+  | 'spacing0'
+  | 'spacing1'
+  | 'spacing2'
+  | 'spacing3'
+  | 'spacing4'
+  | 'spacing5'
+  | 'spacing6'
+  | 'spacing8'
+  | 'spacing10'
+  | 'spacing12'
+  | 'spacing16'
+  | 'spacing20'
+  | 'spacing24'
+  | 'spacing32'
+  | 'spacing40'
+  | 'spacing48'
+  | 'spacing56'
+  | 'spacing64';
+
+export type ElementConfig = {
+  spacing: Spacings;
+};
+
+export interface TypographyConfiguration {
+  heading: ElementConfig;
+  paragraph: ElementConfig;
 }
 
+export const defaultProps = {
+  testId: 'ruids-text-container',
+};
+
 export type TypographyProps = {
-    className?: string
-    children?: React.ReactNode
-    style?: React.CSSProperties
-    testId?: string
-} & typeof defaultProps
+  className?: string;
+  children?: React.ReactNode;
+  style?: React.CSSProperties;
+  testId?: string;
+  configuration?: TypographyConfiguration;
+} & typeof defaultProps;
