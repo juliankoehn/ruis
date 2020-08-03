edd2d4dab545ff3e86707783b7a3cfa87f47bae0
Julian <me@julian.pro>
2020-08-03 19:19:49 +0200

    feat(screens): added screens for media query generation to our tokens

---
+++ packages/tokens/src/tokens/screens.js
@@ -0,0 +1,12 @@
+const screens = {
+    // Small devices (landscape phones, 640px and up)
+    'screen-sm': '640px',
+    // Medium devices (tablets, 768px and up)
+    'screen-md': '768px',
+    // Large devices (desktops, 1024px and up)
+    'screen-lg': '1024px',
+    // Extra large devices (large desktops, 1280px and up)
+    'screen-xl': '1280px'
+};
+
+module.exports = screens;
